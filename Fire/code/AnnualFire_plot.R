7dcde3dad26b85926c53623e0924bd546a0f4fd1
Selmants <selmants@hawaii.edu>
2020-06-25 11:14:37 -0700

    modified Fire graph

--- Fire/code/AnnualFire_plot.R
+++ Fire/code/AnnualFire_plot.R
@@ -4,38 +4,57 @@
 ## R version 3.6.1
 ## 2019-10-26
 
-# Set working directory to "Fire" folder in HI_Model
 # GitHub repository (https://github.com/selmants/HI_Model)
 
 ## This R script is dependent on output from Fire_probabilities.R script ## 
 
-# load ggplot2 version 3.2.0
+# load required R packages
 library(ggplot2)
-# load dplyr version 0.8.3
 library(dplyr)
+library(cowplot)
+
 
 # read statewide annual burned area data into R
-data <- read.csv('AnnualFire.csv', stringsAsFactors = FALSE) 
+data <- read.csv('./Fire/data/processed/AnnualFire.csv', stringsAsFactors = FALSE) 
 
-# sum burned area statewide by year
-statewide <- data %>%
+# line graph of annual area burned statewide
+stateplot <- data %>%
 	group_by(year) %>%
 	summarize(burnarea_km2 = sum(burnarea_km2)) %>%
-	as.data.frame()
+	as.data.frame()%>%
+		{ggplot(., aes(year, burnarea_km2)) +
+		geom_line(color = "#67000D", size = 0.6) +
+		geom_point(color = "#67000D", size = 1.5, shape = 25, fill = "#67000D") +
+		labs(x = "Year",
+		y = expression(Area~burned~(km^{2}))) +
+		scale_x_continuous(limits = c(1998, 2019), breaks = seq(1998, 2018, 4)) +
+		scale_y_continuous(limits = c(0, 175), breaks = seq(0, 175, 25)) +
+		geom_hline(aes(yintercept=28.75), linetype="dashed", size = 0.35) +
+		theme_bw() +
+		theme(panel.grid.minor=element_blank(),
+		axis.title = element_text(size = 10),
+		axis.text = element_text(size = 8))}
 
-# make line graph of annual area burned
-p <- ggplot(statewide, aes(year, burnarea_km2)) +
-	geom_line(color = "#D95F02", size = 0.5) +
-	geom_point(color = "#D95F02", size = 2, shape = 25, fill = "#D95F02") +
-	xlab("Year") +
-	ylab(expression(Area~burned~(km^{2}))) +
-	scale_x_continuous(limits = c(1999, 2019), breaks = seq(2000, 2018, 2)) +
-	scale_y_continuous(limits = c(0, 175), breaks = seq(0, 175, 25)) +
-	geom_hline(aes(yintercept=28.75), linetype="dashed", size = 0.3) +
-	theme(panel.grid.minor=element_blank()) +
-	theme(axis.title.x = element_text(size = 16),
-          axis.text.x  = element_text(size = 12)) +
-	theme(axis.title.y = element_text(size = 16),
-          axis.text.y  = element_text(size = 12))
+# facet plot of annual area burned by island (Hawai, Maui, Oahu, Kauai)
+islandplot <- data %>%
+	filter(island %in% c("Hawaii", "Maui", "Oahu", "Kauai")) %>%
+	{ggplot(., aes(year, burnarea_km2)) +
+		facet_wrap(~ island) +
+		geom_line(color = "#67000D", size = 0.35) +
+		geom_point(color = "#67000D", size = 0.45, shape = 25, fill = "#67000D") +
+		labs(x = "Year",
+			y = expression(Area~burned~(km^{2}))) +
+		scale_x_continuous(limits = c(1998, 2019), breaks = seq(1998, 2018, 5)) +
+		scale_y_continuous(limits = c(0, 138.875), breaks = seq(0, 125, 25)) +
+		theme_bw() +
+		theme(panel.grid.minor = element_blank(),
+			axis.text = element_text(size = 7),
+			axis.title = element_text(size = 9.5))}
+				
+# Create two-panel fire figure
+firepanel <- plot_grid(stateplot, islandplot, 
+	labels = c("A", "B"), rel_widths = c(1.15, 1))
 
-ggsave("./data/processed/HI_AnnualAreaBurned.png", width = 7, height = 3.5)
+# save as .png file 
+ggsave("./Output/manuscript/fig_images/FigS4_AnnualAreaBurned.png",
+ width = 7.5, height = 3)
