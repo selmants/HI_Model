a09eb3785121666d61c2f2bef6974d4b9a3fec24
Selmants <selmants@hawaii.edu>
2021-01-25 16:26:36 -0800

    updated MOD17 R script

--- MOD17_NPP/code/MOD17_processing.R
+++ MOD17_NPP/code/MOD17_processing.R
@@ -17,6 +17,17 @@ library(ggplot2) #version
 ## GitHub repository, https://github.com/selmants/HI_Model 
 
 # create list of annual MOD17 NPP .tif files
-rainlist <- list.files('./MOD17_NPP/data', full.names = TRUE)
+npplist <- list.files('./MOD17_NPP/data', full.names = TRUE)
 #create raster stack of annual NPP .tif files
-nppstack <- stack(npplist)
+stack <- stack(npplist)
+# multiply all values by MODIS scale factor (0.0001)	
+# to get NPP values in kg C m-2 y-1
+npp <- stack * 1e-04
+
+# export pixel NPP data to dataframe
+# and sum values within each year to get 
+# statewide NPP estimates in Tg C y-1
+npp_data <- as.data.frame(npp, xy=TRUE, na.rm = TRUE) %>%
+
+
+
